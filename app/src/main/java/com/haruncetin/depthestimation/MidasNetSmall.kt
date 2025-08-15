5f0e415078f23d94609d902fb38a5e4fe6229d80
abiodunmichal <abiodunmichal909@gmail.com>
2025-08-15 12:53:43 +0100

    Update MidasNetSmall.kt

--- app/src/main/java/com/haruncetin/depthestimation/MidasNetSmall.kt
+++ app/src/main/java/com/haruncetin/depthestimation/MidasNetSmall.kt
@@ -5,7 +5,7 @@ import androidx.camera.core.ExperimentalGetImage
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.Interpreter
 import org.tensorflow.lite.gpu.CompatibilityList
-import org.tensorflow.lite.gpu.GpuDelegateFactory
+import org.tensorflow.lite.gpu.GpuDelegate
 import org.tensorflow.lite.support.common.FileUtil
 import org.tensorflow.lite.support.common.TensorOperator
 import org.tensorflow.lite.support.common.TensorProcessor
@@ -44,8 +44,8 @@ class MidasNetSmall(
         val interpreterOptions = Interpreter.Options().apply {
             val compatibilityList = CompatibilityList()
             if (compatibilityList.isDelegateSupportedOnThisDevice) {
-                val delegateOptions = compatibilityList.bestOptionsForThisDevice
-                this.addDelegate(GpuDelegateFactory(delegateOptions))
+                val delegate = GpuDelegate(compatibilityList.bestOptionsForThisDevice)
+                this.addDelegate(delegate)
             }
             this.numThreads = NUM_THREADS
         }
